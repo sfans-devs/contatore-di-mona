61cba30ab703272d3c321ce5eefa622120308750
StuckDuck <stuck@stuckduck.it>
2022-03-07 14:43:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    unsigned long long mona,cont;
-    cout << "inserire il numero di mona:";
-    cin >> mona;
-    cont = 0;
-    do {
-        cout << "mona" << endl;
+    unsigned long long mona, cont;
+    cout << "inserire il numero di mona: ";
+    cin >> mona; cont = mona; //CDs Nuts
+    while (cont <= mona) {
+        cout << "mona";
         cont++;
-    }while(cont<mona);
+    }
+    cout << "conteggio terminato: risultati";
+    cout << (mona + " mona, " + cont + " iterazioni");
     return 0;
 }
